74db7539b67654f09bd5c7f91483900823f73f52
p0r9 <palathip.opor@gmail.com>
2021-04-24 19:01:32 +0700

    measure science temperature

--- unit converter.py
+++ unit converter.py
@@ -7,13 +7,13 @@ start_window.title('Start Window')
 start_window.geometry('500x300')
 
 #label Title
-ttk.Label(start_window, text = "Collect Unit", background = '#F0FFF0', foreground = '#FD6A02',
-                        font = ("arial", 20)).grid(row = 1, column = 2)
-ttk.Label(start_window,text='',width='18').grid(row=0,column=0)
-ttk.Label(start_window,text='',width='15').grid(row=2,column=2)
-ttk.Label(start_window,text='',width='15').grid(row=5,column=2)
-ttk.Label(start_window,text='',width='15').grid(row=8,column=2)
-ttk.Label(start_window,text='',width='15').grid(row=11,column=2)
+ttk.Label(start_window, text = "Select Unit", background = '#F0FFF0', foreground = '#FD6A02',
+                        font = ("arial", 20)).grid(column = 2, row = 1)
+ttk.Label(start_window,text='',width='18').grid(column=0,row=0)
+ttk.Label(start_window,text='',width='15').grid(column=2,row=2)
+ttk.Label(start_window,text='',width='15').grid(column=2,row=5)
+ttk.Label(start_window,text='',width='15').grid(column=2,row=8)
+ttk.Label(start_window,text='',width='15').grid(column=2,row=11)
 
 #Create Lenght Window
 def lenghtunit_window (event):
@@ -24,13 +24,15 @@ def lenghtunit_window (event):
     #Create Funtion Convert Lenght Unit
     unit_lenght = {'inch' : 0.00052,'keub' : 0.00625,'sok'  :0.0125,'wa' : 0.05,'sen' : 1,'centimaters' : 0.00025,
                    'maters' : 0.025,'kilomaters' : 25,'yot' : 400}
+
     unit_lenght_dict = {'inch' : 'นิ้ว','keub' : 'คืบ','sok' : 'ศอก','wa' : 'วา','sen' : 'เส้น',
                         'centimaters' : 'เซนติเมตร','maters' : 'เมตร','kilomaters' : 'กิโลเมตร','yot' : 'โยชน์'}
+
     def convert_lenght (event):
         unit_ch = unit_choosen.get()
         unit_con_ch = unit_converted_choosen.get()
         user_input = int(input_entry.get())
-        resalt_label.configure(text=(str((user_input*unit_lenght[unit_ch])/unit_lenght[unit_con_ch])) + ' '
+        resalt_label.configure(text=(str('%.2f'%((user_input*unit_lenght[unit_ch])/unit_lenght[unit_con_ch]))) + ' '
                                + unit_lenght_dict[unit_con_ch])
     
     #Create Clear Funtion
@@ -88,7 +90,7 @@ def lenghtunit_window (event):
 #Create Lenght Button
 thai_to_matric_lenghtbutton = Button(start_window,text='ThaiUnit to MatricUnit Lenght',font=('arial,7'),
                               background = '#FFF0F5',foreground='#1C2951',width='25')
-thai_to_matric_lenghtbutton.grid(row=4,column=2)
+thai_to_matric_lenghtbutton.grid(column=2,row=4)
 thai_to_matric_lenghtbutton.bind('<Button-1>',lenghtunit_window)
 
 
@@ -96,14 +98,53 @@ thai_to_matric_lenghtbutton.bind('<Button-1>',lenghtunit_window)
 def measureunit_window (event):
     measureunit_window = Tk()
     measureunit_window.title('ThaiUnit to MatricUnit Measure Window')
-    measureunit_window.geometry('700x300')
+    measureunit_window.geometry('740x300')
+
+    unit_measure  = {'ban' : 220,'kwian' : 440,'thang'  : 4.4 ,'liters' : 0.22 ,
+                     'cubic centimaters' : 0.00022,'gallon(Br.)' : 1,'cubic maters' : 220,
+                     'pound' : 0.128205,'kilogram' : 0.28205 ,'gram' : 0.00028205,'ounce': 0.00801}
+
+    unit_measure_dict = {'ban' : 'บั้น' ,'kwian' : 'เกวียน' ,'thang'  : 'ถัง' ,'liters' : 'ลิตร' ,
+                        'cubic centimaters' : 'ลูกบาศก์เซนติเมตร' ,'gallon(Br.)' : 'แกลลอน' ,
+                        'cubic maters' : 'ลูกบาศก์เมตร' ,'pound' : 'ปอนด์' ,'kilogram' : 'กิโลกรัม' ,
+                        'gram' : 'กรัม' ,'ounce' : 'ออนซ์'}
+
+    def convert_measure (event):
+        unit_ch = unit_choosen.get()
+        unit_con_ch = unit_converted_choosen.get()
+        user_input = int(input_entry.get())
+        resalt_label.configure(text=(str('%.3f'%((user_input*unit_measure[unit_ch])/unit_measure[unit_con_ch]))) + ' '
+                               + unit_measure_dict[unit_con_ch])
+    
+    #Create Clear Funtion
+    def clear_unit_measure (event) :
+        unit_choosen.set('')
+        unit_converted_choosen.set('')
+        resalt_label.configure(text = '0')
+        input_entry.delete(0,END)
+
     #Label for Title
     ttk.Label(measureunit_window, text = 'ThaiUnit to MatricUnit Measure', background = '#F0FFF0',
-                                 foreground ="#FD6A02", font = ("arial", 17)).grid(row = 0, column = 1)
+                                   foreground ="#FD6A02", font = ("arial", 17)).grid(column = 1, row = 0)
     ttk.Label(measureunit_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 2 )    
     ttk.Label(measureunit_window, text = "Select Unit : ",font = ("arial", 11)).grid(column = 0,row = 5)
-    #ttk.Label(unitlenght_window, text = " to ",font = ("arial", 11)).grid(column = 1,row = 5 )
+    ttk.Label(measureunit_window, text = " to ",font = ("arial", 11),width=10).grid(column = 2,row = 5 )
     ttk.Label(measureunit_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 6 ) 
+
+    #Comboblox ThaiUnit
+    n = StringVar()
+    unit_choosen = ttk.Combobox(measureunit_window, width = 27, textvariable = n)
+    unit_choosen ['values'] = ('ban','kwian','thang','liters','cubic centimaters','gallon(Br.)','cubic maters',
+                               'pound','kilogram','gram','ounce')
+    unit_choosen.grid(column = 1 , row = 5)
+    unit_choosen.current() 
+
+    n = StringVar()
+    unit_converted_choosen = ttk.Combobox(measureunit_window, width = 27, textvariable = n)
+    unit_converted_choosen ['values'] = ('ban','kwian','thang','liters','cubic centimaters','gallon(Br.)',
+                                         'cubic maters','pound','kilogram','gram','ounce')
+    unit_converted_choosen.grid(column = 4 , row = 5)
+    unit_converted_choosen.current()
 
     #Input Lenght   
     input_label = ttk.Label(measureunit_window, text = " Input Your Number : ",font = ("arial", 11))
@@ -112,21 +153,27 @@ def measureunit_window (event):
     input_entry = ttk.Entry (measureunit_window,width='30')
     input_entry.grid(column = 1,row = 7 )
 
-    ttk.Label(measureunit_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 8 )    
+    ttk.Label(measureunit_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 8 )  
+    ttk.Label(measureunit_window, text = " to ",font = ("arial", 11),width=10).grid(column = 2,row = 7 )
+
+    resalt_label = ttk.Label(measureunit_window, text = "0",font = ("arial", 11))
+    resalt_label.grid(column = 4,row = 7 )
 
     enter_button = Button(measureunit_window, text = " Enter ",font = ("arial", 11),
                                   background = 'red',foreground ="white")
     enter_button.grid(column = 0,row = 9 )
+    enter_button.bind('<Button-1>',convert_measure)
 
     clear_button = Button(measureunit_window, text = " Clear ",font = ("arial", 11))
     clear_button.grid(column = 1,row = 9 )
+    clear_button.bind('<Button-1>',clear_unit_measure)
 
     measureunit_window.mainloop()
 
 #Create Measure Button
 thai_to_matric_measurebutton = Button(start_window,text='ThaiUnit to MatricUnit Measure ',font=('arial,7'),
                               background = '#FFF0F5',foreground='#1C2951',width='25')
-thai_to_matric_measurebutton.grid(row=7,column=2)
+thai_to_matric_measurebutton.grid(column = 2 , row = 7)
 thai_to_matric_measurebutton.bind('<Button-1>',measureunit_window)
 
 
@@ -134,38 +181,96 @@ thai_to_matric_measurebutton.bind('<Button-1>',measureunit_window)
 def scienceunit_window (event):
     scienceunit_window = Tk()
     scienceunit_window.title('ScienceUnit Window')
-    scienceunit_window.geometry('700x300')
+    scienceunit_window.geometry('500x300')
 
-    #Label for Title
-    ttk.Label(scienceunit_window, text = 'Convented Science Unit', background = '#F0FFF0',
-                                 foreground ="#FD6A02", font = ("arial", 17)).grid(column=1,row=0)
-    ttk.Label(scienceunit_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 2 )    
-    ttk.Label(scienceunit_window, text = "Select Unit : ",font = ("arial", 11)).grid(column = 0,row = 5)
-    #ttk.Label(unitlenght_window, text = " to ",font = ("arial", 11)).grid(column = 1,row = 5 )
-    ttk.Label(scienceunit_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 6 ) 
+    #label Title
+    ttk.Label(scienceunit_window, text = "Select Unit", background = '#F0FFF0', foreground = '#FD6A02',
+                            font = ("arial", 20)).grid(column = 2, row = 1) 
+    ttk.Label(scienceunit_window,text='',width='18').grid(column=0,row=0)
+    ttk.Label(scienceunit_window,text='',width='15').grid(column=2,row=2)
+    ttk.Label(scienceunit_window,text='',width='15').grid(column=2,row=5)
+    ttk.Label(scienceunit_window,text='',width='15').grid(column=2,row=8)
+    ttk.Label(scienceunit_window,text='',width='15').grid(column=2,row=11)
 
-    #Input Lenght   
-    input_label = ttk.Label(scienceunit_window, text = " Input Your Number : ",font = ("arial", 11))
-    input_label.grid(column = 0,row = 7 )
+    def temperature_window(event) :
+        temperature_window = Tk()
+        temperature_window.title('ThaiUnit to MatricUnit Measure Window')
+        temperature_window.geometry('740x300')
+    
+        unit_temperature  = {'Celsius' : 1 , 'Fahrenheit' : -17.22, 'Kelvin' : -272.15 , 'Réaumur' : 1.25 }
 
-    input_entry = ttk.Entry (scienceunit_window,width='30')
-    input_entry.grid(column = 1,row = 7 )
+        unit_temperature_dict = {'Celsius' : '°C' , 'Fahrenheit' : '°F' , 'Kelvin' : 'K' , 'Réaumur' : '°R'}
 
-    ttk.Label(scienceunit_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 8 )    
+        def convert_temperature (event):
+            unit_ch = unit_choosen.get()
+            unit_con_ch = unit_converted_choosen.get()
+            user_input = int(input_entry.get())
+            resalt_label.configure(text=(str('%.3f'%((user_input*unit_temperature[unit_ch])/unit_temperature[unit_con_ch]))) + ' '
+                                   + unit_temperature_dict[unit_con_ch])
+    
+        #Create Clear Funtion
+        def clear_unit_temperature (event) :
+            unit_choosen.set('')
+            unit_converted_choosen.set('')
+            resalt_label.configure(text = '0')
+            input_entry.delete(0,END)
 
-    enter_button = Button(scienceunit_window, text = " Enter ",font = ("arial", 11),
-                                  background = 'red',foreground ="white")
-    enter_button.grid(column = 0,row = 9 )
+        #Label for Title
+        ttk.Label(temperature_window, text = 'ThaiUnit to MatricUnit Measure', background = '#F0FFF0',
+                                      foreground ="#FD6A02", font = ("arial", 17)).grid(column = 1, row = 0)
+        ttk.Label(temperature_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 2 )    
+        ttk.Label(temperature_window, text = "Select Unit : ",font = ("arial", 11)).grid(column = 0,row = 5)
+        ttk.Label(temperature_window, text = " to ",font = ("arial", 11),width=10).grid(column = 2,row = 5 )
+        ttk.Label(temperature_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 6 ) 
 
-    clear_button = Button(scienceunit_window, text = " Clear ",font = ("arial", 11))
-    clear_button.grid(column = 1,row = 9 )
+        #Comboblox ThaiUnit
+        n = StringVar()
+        unit_choosen = ttk.Combobox(temperature_window, width = 27, textvariable = n)
+        unit_choosen ['values'] = ('Celsius','Fahrenheit','Kelvin','Réaumur')
+        unit_choosen.grid(column = 1 , row = 5)
+        unit_choosen.current() 
+
+        n = StringVar()
+        unit_converted_choosen = ttk.Combobox(temperature_window, width = 27, textvariable = n)
+        unit_converted_choosen ['values'] = ('Celsius','Fahrenheit','Kelvin','Réaumur')
+        unit_converted_choosen.grid(column = 4 , row = 5)
+        unit_converted_choosen.current()
+
+        #Input Lenght   
+        input_label = ttk.Label(temperature_window, text = " Input Your Number : ",font = ("arial", 11))
+        input_label.grid(column = 0,row = 7 )
+
+        input_entry = ttk.Entry (temperature_window,width='30')
+        input_entry.grid(column = 1,row = 7 )
+
+        ttk.Label(temperature_window, text = "  ",font = ("arial", 11)).grid(column = 0,row = 8 )  
+        ttk.Label(temperature_window, text = " to ",font = ("arial", 11),width=10).grid(column = 2,row = 7 )
+
+        resalt_label = ttk.Label(temperature_window, text = "0",font = ("arial", 11))
+        resalt_label.grid(column = 4,row = 7 )
+
+        enter_button = Button(temperature_window, text = " Enter ",font = ("arial", 11),
+                                                  background = 'red',foreground ="white")
+        enter_button.grid(column = 0,row = 9 )
+        enter_button.bind('<Button-1>',convert_temperature)
+
+        clear_button = Button(temperature_window, text = " Clear ",font = ("arial", 11))
+        clear_button.grid(column = 1,row = 9 )
+        clear_button.bind('<Button-1>',clear_unit_temperature)
+
+        #Create Temperature Button
+    temperaturebutton = Button(scienceunit_window,text='Temperature Convert',font=('arial,7'),
+                                                  background = '#FFF0F5',foreground='#1C2951',width='25')
+    temperaturebutton.grid(column=2,row=4)
+    temperaturebutton.bind('<Button-1>',temperature_window)
 
     scienceunit_window.mainloop()
     
 #Create Science Button
-science_button = Button(start_window,text='Convented Science Unit',font=('arial,7'),width='25',
+science_button = Button(start_window,text='Convert Science Unit',font=('arial,7'),width='25',
                      background = '#FFF0F5',foreground='#1C2951' )
 science_button.grid(row=10,column=2)
+
 science_button.bind('<Button-1>',scienceunit_window)
 
 
